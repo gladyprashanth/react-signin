e9d6b7011e763a5300e051a3abb7562c00c96654
ChandruLoganathanChandru <chandru@pixalive.me>
2023-11-25 15:59:11 +0530

    working
    on the initial setup

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import './Header.css'
 import LogoIcon from '../../Assets/LogoIcon.webp'
+import { BiSupport } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+
 
 const Header = () => {
     return (
         <div className='Header'>
             <div className='topHeader'>
-                <div>
-                    <img
-                        src={LogoIcon}
-                    />
+                <div className='iconAndSearchContainer'>
+                    <div>
+                        <img
+                            src={LogoIcon}
+                        />
+                    </div>
+                    <div className='headerInput'>
+                        <input
+                            placeholder='Enter your search'
+                        />
+                    </div>
                 </div>
-                <div className=''>
-                    <input
-                        placeholder='Enter your search'
-                    />
+                <div className='contactUsHeader'>
+                    <div className='contactUsHeaderContainer'>
+                        <div className='contactUsIcon'>
+                            <BiSupport />
+                        </div>
+                        <div className='contactUsContent'>
+                            <div>Call Us Now</div>
+                            <div>9789724829</div>
+                        </div>
+                    </div>
+                    <div className='contactUsHeaderContainer'>
+                        <div className='contactUsIcon'>
+                            <CgProfile />
+                        </div>
+                        <div className='contactUsContent'>
+                            <div>Call Us Now</div>
+                            <div>chandru@pixalive.me</div>
+                        </div>
+                    </div>
                 </div>
-                <div>call us</div>
-                <div>Email Us</div>
             </div>
             <div>Dropdown Header</div>
         </div>
