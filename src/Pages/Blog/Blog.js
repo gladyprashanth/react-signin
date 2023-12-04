f7c2d62c6989ae6252938cb35ee4087d2ee4fbd1
vigneshenbarasan <vigneshenbarasan@pixalive.me>
2023-12-04 09:51:09 +0530

    ccc

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -151,9 +151,8 @@ const Blog = () => {
   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
 </svg> {blog.date}</span></p>
-          
-            <div className="card-body">
-              <h5 className="card-title text-start fs-4">{blog.title}</h5>
+              <div className="card-body">
+              <h5 className="card-title text-start fs-4 fix">{blog.title}</h5>
               <p className="card-text text-start text-secondary">{blog.content}</p>
             </div>
             <div className="card-body">
