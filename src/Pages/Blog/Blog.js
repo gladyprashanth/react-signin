9b8d467b659605fa924791e924623794f7fedc96
vigneshenbarasan <vigneshenbarasan@pixalive.me>
2023-12-06 11:45:39 +0530

    small blog page change

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -138,7 +138,7 @@ const Blog = () => {
       {blogData.map((blog) => (
         <div key={blog.id} className='col-sm-4 mt-5  '>
           <div className="card search " style={{ width: "100" }}>
-           <a href='' ><img  src={blog.imageSrc} className="card-img-top img-fluid p-2" alt="..." /> </a>
+           <a href='' ><img  src={blog.imageSrc} className="card-img-top img-fluid p-2 " alt="..." /> </a>
             <div className="boot_icon ">
                                     <div className="round fs-4  ">
                                    <a href=''> <svg className='  text-black ' xmlns="http://www.w3.org/2000/svg" width="40" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
@@ -153,9 +153,9 @@ const Blog = () => {
              </div>
              </div>
             
-            <p className='text-start detail mt-3'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <p className='text-start detail mt-3 mx-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"/>
-</svg> Rental plazza on <span className='mx-3'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+</svg> Rental plazza on <span className='mx-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
 </svg> {blog.date}</span></p>
